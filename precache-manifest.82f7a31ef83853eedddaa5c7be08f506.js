7ca8449e2f4a63d057dd87ab40b1a9a378e32fef
dajiaman <dajiaman@live.com>
2021-03-10 19:14:08 +0000

    Automatically update from travis-ci

---
+++ precache-manifest.82f7a31ef83853eedddaa5c7be08f506.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4e9b2e2d5481a768940",
+    "url": "/youzan/css/app.e24ec8db.css"
+  },
+  {
+    "revision": "b95152c56ba49b9dcd34",
+    "url": "/youzan/css/chunk-1744855b.23bf0383.css"
+  },
+  {
+    "revision": "8bb8bb2f943e5cb05527",
+    "url": "/youzan/css/chunk-1ffceda9.bcda79f9.css"
+  },
+  {
+    "revision": "257e94485ab3623e195e",
+    "url": "/youzan/css/chunk-de23b9e0.8c72a468.css"
+  },
+  {
+    "revision": "932f9e8f12e21599bd23",
+    "url": "/youzan/css/chunk-vendors.52db998c.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/youzan/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "0de0412ea3e12033ed4894a50f0ca85a",
+    "url": "/youzan/index.html"
+  },
+  {
+    "revision": "595a54f2ae912373ed81",
+    "url": "/youzan/js/about.919e96da.js"
+  },
+  {
+    "revision": "c4e9b2e2d5481a768940",
+    "url": "/youzan/js/app.148dae60.js"
+  },
+  {
+    "revision": "b95152c56ba49b9dcd34",
+    "url": "/youzan/js/chunk-1744855b.215d5df7.js"
+  },
+  {
+    "revision": "8bb8bb2f943e5cb05527",
+    "url": "/youzan/js/chunk-1ffceda9.7913f76d.js"
+  },
+  {
+    "revision": "735ce0f5c71ccfb98a28",
+    "url": "/youzan/js/chunk-2d0de395.eb8a53ec.js"
+  },
+  {
+    "revision": "491c02f473eab0ef9b89",
+    "url": "/youzan/js/chunk-512f76de.ad85cfde.js"
+  },
+  {
+    "revision": "257e94485ab3623e195e",
+    "url": "/youzan/js/chunk-de23b9e0.8cc97a6f.js"
+  },
+  {
+    "revision": "932f9e8f12e21599bd23",
+    "url": "/youzan/js/chunk-vendors.87339e56.js"
+  },
+  {
+    "revision": "3f6bb87c4086417649544dbe9e14b743",
+    "url": "/youzan/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/youzan/robots.txt"
+  }
+]);
